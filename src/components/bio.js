88e870042afae5c8e3d4bcbdd46063c3a37ed5f1
Shaundai Person <shaundaiperson@gmail.com>
2020-04-03 22:39:06 -0400

    update assets and author

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,11 +34,10 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong>.  Mom of 1. MBA. Loves long #sociallydistant walks down empty grocery aisles.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                Follow her on Twitter
               </a>
             </p>
           </Container>
