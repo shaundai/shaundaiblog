1e9dc6e79f2bc391fcd3de97777064b59d8186bc
Shaundai Person <shaundaiperson@gmail.com>
2020-04-03 22:26:56 -0400

    update blog, learn gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Shaundai's Blog`,
+    author: `Shaundai`,
+    description: `A personal blog with styled components, dark mode, and Netlify CMS.`,
+    siteUrl: `https://blog.shaundai.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `shaundai`,
     },
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,8 @@ class IndexPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+          title="Blog"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`,  `shaundai`, `shaundai person`]}
         />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
@@ -22,12 +22,10 @@ class IndexPage extends React.Component {
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
         <p>
           This starter comes out of the box with styled components and Gatsby's
           default starter blog running on Netlify CMS.
         </p>
-        <p>Now go build something great!</p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
