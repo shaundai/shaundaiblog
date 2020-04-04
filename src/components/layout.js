8447d318701ed378cc2789c71d68d5b383712866
Shaundai Person <shaundaiperson@gmail.com>
2020-04-04 14:51:24 -0400

    edit social

--- src/components/layout.js
+++ src/components/layout.js
@@ -67,7 +67,7 @@ class Layout extends React.Component {
           <main>{children}</main>
         </div>
         <Footer>
-        <a href={`https://twitter.com/${social.twitter}`}>twitter</a> | 
+        <a href={`https://twitter.com/shaundai`}>twitter</a> | 
         <br></br>
           © {new Date().getFullYear()} Shaundai Person
         </Footer>
